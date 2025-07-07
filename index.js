df3b1f83c3427b1de32240156a7f9921f139c7e7
gonzalosusel <48589225@est.ort.edu.ar>
2025-07-07 10:39:22 -0300

    Terminar primeros 5 puntos, comenzar el 6

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const client = new Client({
 
 client.connect();
 const app = express();
+const secretkey = "vendo brownies vendo brownies 12345678 ay AY $!$&&/$#";
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -33,7 +34,7 @@ app.get("/api/event", async(req, res) => {
 
 app.get("/api/event/:id", async(req, res) => {
     const resp = (await client.query("select * from events e inner join event_locations el on e.id_event_location=el.id inner join locations l on el.id_location=l.id inner join provinces p on l.id_province=p.id inner join users u on e.id_creator_user=u.id")).rows;
-    res.json(resp);
+    res.json(resp[0]);
 });
 
 app.post("/api/user/login", async(req, res) => {
@@ -62,7 +63,6 @@ app.post("/api/user/login", async(req, res) => {
         id: resp[0].id
     };
 
-    const secretkey = "vendo brownies vendo brownies 12345678 ay AY $!$&&/$#";
     const options = {
         expiresIn: "3h",
         issuer: "TP Susel Bebczuk Wilder"
@@ -92,7 +92,54 @@ app.post("/api/user/register", async(req, res) => {
         return;
     }
 
-    await client.query(`INSERT INTO users(first_name, last_name, username, password) VALUES ('${first_name}', '${last_name}', '${username}', '${password}')`);
+    await client.query(`INSERT INTO users(first_name, last_name, username, password) OUTPUT id VALUES ('${first_name}', '${last_name}', '${username}', '${password}')`);
+    res.status(201).send();
+});
+
+app.post("/api/event", async(req, res) => {
+    const {
+        name,
+        description,
+        start_date,
+        duration_in_minutes,
+        price,
+        enabled_for_enrollment,
+        max_assistance,
+        location,
+        full_address,
+        max_capacity,
+        latitude,
+        longitude,
+        id_province,
+        token
+    } = req.body;
+
+    let idusuario;
+    try{
+        idusuario = jwt.verify(token, secretkey);
+    } catch{
+        res.status(401).json({ error: "Usuario no autenticado" });
+        return;
+    }
+
+    if(!name || !description || name.length < 3 || description < 3){
+        res.status(400).json({ error: "Nombre o descripción inválida" });
+        return;
+    }
+
+    if(max_assistance > max_capacity){
+        res.status(400).json({ error: "La asistencia máxima excede la capacidad máxima" });
+        return;
+    }
+
+    if(price < 0 || duration_in_minutes < 0){
+        res.status(400).json({ error: "El precio o la duración son menores que cero" });
+        return;
+    }
+
+    await client.query(`INSERT INTO locations(name, id_province, latitude, longitude) VALUES ('${location}', ${id_province}, ${latitude}, ${longitude})`);
+    //await client.query("INSERT INTO event_locations(id_location, name, full_adress, max_capacity, latitude, longitude, id_creator_user) VALUES (?, ?, ?, ?, ?, ?, ?);");
+
     res.status(201).send();
 });
 
