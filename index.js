8f881038c8e4337d968fcaf2a9f446bdbaf5975e
gonzalosusel <48589225@est.ort.edu.ar>
2025-07-08 09:53:51 -0300

    Terminar API

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import auth from "./routes/auth.js";
 import events_crud from "./routes/events_crud.js";
 import events_enrollment from "./routes/events_enrollment.js";
+import events_location from "./routes/events_location.js";
 import events from "./routes/events.js";
 
 const app = express();
@@ -14,5 +15,6 @@ app.use(auth);
 app.use(events_crud);
 app.use(events);
 app.use(events_enrollment);
+app.use(events_location);
 
 app.listen(3128, () => console.log("Corriendo")); 
---
+++ routes/events_location.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import client from "../bd.js";
+
+const router = Router();
+const secretkey = "vendo brownies vendo brownies 12345678 ay AY $!$&&/$#";
+
+router.get("/api/event-location", async(req, res) => {
+    const { token } = req.body;
+
+    let idusuario;
+    try{
+        idusuario = parseInt(jwt.verify(token, secretkey).id);
+    } catch{
+        res.status(401).json({ error: "Usuario no autenticado" });
+        return;
+    }
+
+    let data = (await client.query("SELECT el.name, el.max_capacity, el.full_address, el.latitude, el.longitude FROM event_enrollments ee INNER JOIN events e ON ee.id_event=e.id INNER JOIN event_locations el ON e.id_event_location=el.id WHERE ee.id_user=$1", [idusuario])).rows;
+    res.status(200).send(data);
+});
+
+router.get("/api/event-location/:id", async(req, res) => {
+    const { token } = req.body;
+
+    let idusuario;
+    try{
+        idusuario = parseInt(jwt.verify(token, secretkey).id);
+    } catch{
+        res.status(401).json({ error: "Usuario no autenticado" });
+        return;
+    }
+
+    const event = (await client.query("SELECT * FROM events WHERE id=$1", [req.params.id])).rows;
+    if(event.length == 0){
+        res.status(404).json({ error: "Evento no encontrado" });
+        return;
+    }
+
+    let data = (await client.query("SELECT el.name, el.max_capacity, el.full_address, el.latitude, el.longitude FROM event_enrollments ee INNER JOIN events e ON ee.id_event=e.id INNER JOIN event_locations el ON e.id_event_location=el.id WHERE ee.id_user=$1 AND ee.id_event=$2", [idusuario, req.params.id])).rows[0];
+    res.status(200).send(data);
+});
+
+export default router;
